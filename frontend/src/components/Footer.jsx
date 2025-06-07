ac0d4b3de3a51fd1ed6e65fda9e7900997fcc5cf
Aashirwad2nd <akfun6033@gmail.com>
2025-06-07 08:18:55 +0545

    Logo changed for Recipe

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,17 +7,11 @@ function Footer() {
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            viewBox="0 0 64 64"
             className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            fill="currentColor"
           >
-            <path d="M4 3v7a2 2 0 002 2v9" /> {/* Fork */}
-            <path d="M8 3v7a2 2 0 01-2 2v9" />
-            <path d="M20 5h-2a2 2 0 00-2 2v2a5 5 0 004 4.9V21" /> {/* Spoon */}
+            <path d="M48 24c0-6.6-5.4-12-12-12-2.2 0-4.3.6-6 1.6C27.3 11.6 25.2 11 23 11c-6.6 0-12 5.4-12 12 0 2.7 1 5.2 2.6 7.2C10.2 31.2 8 34.4 8 38c0 5 4 9 9 9h30c5 0 9-4 9-9 0-3.6-2.2-6.8-5.6-6.8 1.6-2 2.6-4.5 2.6-7.2zM16 47h32v5H16z" />
           </svg>
           <span className="ml-3 text-xl">Recipe</span>
         </a>
