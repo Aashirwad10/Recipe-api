36fac116e8f5ba9441e80608fc7c72079e22df4c
Aashirwad2nd <akfun6033@gmail.com>
2025-06-07 08:54:20 +0545

    Added App.css in header and home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import pp from '../assets/pp.png';
+import "../App.css";
 
 
 function Home() {
